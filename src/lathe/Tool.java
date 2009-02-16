81f82113aa6066c1d815e88ffaece4357a3a45b8
christopherbecker@stmarksschool.org <christopherbecker@stmarksschool.org@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-02-16 15:46:43 +0000

    Created Tool and VisualTool mock-ups, started to document future changes.

--- src/lathe/Tool.java
+++ src/lathe/Tool.java
@@ -1,5 +1,9 @@
 package lathe;
 
+import java.util.*;
+
+import simplerjogl.Vertex;
+
 /**
  * Tool
  * 
@@ -9,7 +13,49 @@ package lathe;
  * 
  * @author Seth Battis
  */
-public class Tool
+public class Tool implements Iterable<Vertex>
 {
+	protected ArrayList<Vertex> surface;
 
+	public Tool()
+	{
+		surface = new ArrayList<Vertex>();
+		
+		// these are totally arbitrary values!
+		surface.add (new Vertex (1, 2));
+		surface.add (new Vertex (1.3, 1.4));
+		surface.add (new Vertex (1.71, 3.14159));
+	}
+	
+	/* TODO Things to add
+	 * 
+	 * Need to be able to move the tool (a method to do this?)
+	 * 
+	 * Need to be able to create vertices at the intersection of the tool edges and the work piece edges
+	 * 
+	 * Need to be able to add vertices to the workpiece to represent cuts (this may require updating the workpiece as well)
+	 */
+	
+	public Iterator<Vertex> iterator ()
+	{
+		final Iterator<Vertex> surfaceIterator = surface.iterator ();
+		return new Iterator<Vertex> ()
+		{
+
+			public boolean hasNext ()
+			{
+				return surfaceIterator.hasNext ();
+			}
+
+			public Vertex next ()
+			{
+				return new Vertex (surfaceIterator.next ());
+			}
+
+			public void remove ()
+			{}
+
+		};
+	}
 }
+
