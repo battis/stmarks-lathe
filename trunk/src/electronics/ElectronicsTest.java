d40f7acf390abf7c58df9f8a3690c17c20d088d9
battis <battis@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-03-06 17:40:23 +0000

    Latest electronics testing sequence (with beeps, yet!)

--- trunk/src/electronics/ElectronicsTest.java
+++ trunk/src/electronics/ElectronicsTest.java
@@ -1,36 +1,60 @@
 package electronics;
 
+import java.awt.*;
+
 import arduino.*;
 
-@SuppressWarnings("serial")
 public class ElectronicsTest extends arduino.Arduino
 {	
+	private static final long serialVersionUID = 1L;
+	private int testCount;
+	private Timer timer = new Timer (3000);
+	private Toolkit tool = Toolkit.getDefaultToolkit ();
+
 	public void setup()
 	{
-		super.setup();
 		System.out.println ("Starting Electronics Testing sequence");
+		super.setup();
+		testCount = 0;
+	}
+
+	private void portWrite (char c)
+	{
+		port.write(c);
+		tool.beep();
+		timer.reset();
+		while (timer.isRunning()) {}
 	}
 	
 	public void draw ()
 	{
-		Timer t = new Timer (3000);
-		
+
 		// sending instructions about how to control the lathe
 		// collecting information about the encoders
 		// communicating with the rest of the lathe software on the computer
-		
-		System.out.println ("Testing RCW");
-		port.write('A');
-	
-		t.reset();
-		while (t.isRunning()) {}
-		
-		/* disabled until wiring is connected
-		System.out.println ("Testing RCCW");
-		port.write('B');
-		
-		t.reset();
-		while (t.isRunning()) {}*/
 
+		if (testCount < 2)
+		{
+			testCount++;
+			
+			System.out.println ("Testing RCW");
+			portWrite('A');
+
+			System.out.println ("Testing RCW");
+			portWrite('A');
+
+			System.out.println ("Testing RCCW");
+			portWrite('B');
+
+			System.out.println ("Testing Zero Power");
+			portWrite ('C');
+		}
+		else if (testCount == 2)
+		{
+			System.out.println ("Ending Electronics Testing sequence");
+			tool.beep();
+			testCount++;
+			System.exit (0);
+		}
 	}
 }
