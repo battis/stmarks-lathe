754958dfe16287cf6bd0dc82c9b0e75e0b4ff716
battis <battis@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-04-17 00:04:33 +0000

    Fiddled with EncoderTest

---
+++ trunk/src/electronics/EncoderTest/applet/EncoderTest.cpp
@@ -0,0 +1,40 @@
+#include "WProgram.h"
+void setup ();
+void loop();
+int XENC1 = 0;
+int LED = 13;
+
+char ARDUINO_HANDSHAKE = '@';
+
+void setup ()
+{
+  Serial.begin (9600);
+
+  /* send handshake to computer */
+  Serial.print (ARDUINO_HANDSHAKE);
+  
+  pinMode (XENC1, INPUT);
+  pinMode (LED, OUTPUT);
+}
+
+void loop()
+{
+  int irvalue = analogRead (XENC1);
+  Serial.print (millis(), DEC);
+  Serial.print ("ms: ");
+  Serial.print (irvalue, DEC);
+  Serial.println ("/1024 * 5V");
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
