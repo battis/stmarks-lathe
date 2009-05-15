e6fbcc21e12764c62c671f5cf73b81f5bed4288b
Alexanderngan@stmarksschool.org <Alexanderngan@stmarksschool.org@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-05-15 17:14:22 +0000

    ok, what happened here, is that I have updated the parser a little bit, a new approach of the g01

--- src/gcode/Parser.java
+++ src/gcode/Parser.java
@@ -119,16 +119,13 @@ public class Parser
 		/*
 		 * if(G-code == 01)
 		 * {
-		 * (y(destination)-y(current position))==y;
-		 * (x(destination)-x(current position))==x;
-		 *  turn xknob 187*x @power
-		 *  turn yknob 187*y @power
-		 *  	horizontal = yknob @power 0, xknob @ power 255; 
-		 *  	vertical = xknob @power 0, yknob @ power 255; 
-		 *  	one degree towards vertical up til 45 degrees, yknob increases by 255/90 power
-		 *  	after 45 degrees, xknob decreases 255/90 power, while yknob stays at 255
-		 *  to find the angle, acrtan(y/x)
-		 * }
+		 * for(int i=0;i<length of the object; i++)
+		 * (y(xcurrent)-y(xold))==y;
+		 * x==1
+		 *  turn xknob 187*x 
+		 *  turn yknob 187*y 
+		 *  }
+		 *  }
 		 */
 	}
 }
