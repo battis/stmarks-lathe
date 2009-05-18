c2bb9a3fb264a65b874af0b839dfeef818ac35d3
Alexanderngan@stmarksschool.org <Alexanderngan@stmarksschool.org@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-05-18 14:42:43 +0000

    g1 is starting to look good!~

--- trunk/src/gcode/Translator.java
+++ trunk/src/gcode/Translator.java
@@ -14,7 +14,7 @@ public class Translator extends Arduino
 	{
 		/* do all the things that the superclass(es) do(es) in its constructor */
 		super();
-		
+
 		/* create a new Parser object based on whatever the user chooses in the file chooser dialog, parse it and get ready to translate it. */
 		Parser p = null;
 		JFileChooser fc = new JFileChooser ();
@@ -36,33 +36,65 @@ public class Translator extends Arduino
 		{
 			System.exit (0);
 		}
-		
+
 		/* if we get this far, we've successfully parsed something -- print it out! */
 		System.out.println (p);
-		
+
 		/* store a reference to the parsed program in this object for future translation */
 		program = p.getProgram();
 	}
-	
+
 	public void translate ()
 	{
+		double x = 0, z = 0;
+		double newx, newz;
 		for (Block b : program)
 		{
 			if(b.getCommandSet() == 'G' && b.getCommand() == 1)
 			{
-				
+				for (Segment s : b)
+				{
+					if (s.getLetter() == 'Z')
+					{
+						newz = s.getNumber();
+						if (newz - z > 0)
+						{
+							for (double i = z; i < newz; i ++)
+							{
+								RCW (255,(int) ((newz - z) * 187));
+							}
+						}
+						else if (newz - z < 0)
+						{
+							for (double i = z; i < newz; i ++)
+							{
+								RCCW (255,(int) ((newz - z) * 187));
+							}
+						}
+						z = newz;
+					}
+					if (s.getLetter() == 'X')
+					{
+						newx = s.getNumber();
+						if (newx - x > 0)
+						{
+							for (double i = x; i < newx; i ++)
+							{
+								RCW ((int)((newx - x) * 187) , 255);
+							}
+						}
+						else if (newx - x < 0)
+						{
+							for (double i = x; i < newx; i ++)
+							{
+								RCCW ((int)((newx - x) * 187) , 255);
+							}
+						}
+						x = newx;
+					}
+				}
+
 			}
-		/*
-		 *
-		 * {
-		 * for(int i=0;i<length of the object; i++)
-		 * (y(xcurrent)-y(xold))==y;
-		 * x+=1
-		 *  turn xknob 187*x 
-		 *  turn yknob 187*y 
-		 *  }
-		 *  }
-		 */
 		}
 	}
 }
