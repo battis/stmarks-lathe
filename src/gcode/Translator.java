54a8b9f046ea3dd28a99391c9cb61bfab8ce024e
Alexanderngan@stmarksschool.org <Alexanderngan@stmarksschool.org@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-05-19 14:25:43 +0000

    good stuff-- documentation

--- src/gcode/Translator.java
+++ src/gcode/Translator.java
@@ -46,66 +46,80 @@ public class Translator extends Arduino
 
 	public void translate ()
 	{
-		double x = 0, z = 0;
-		double newx, newz;
 		for (Block b : program)
 		{
-			//read in g-code segment. If G-code is g1 (linear), than do the following.
 			if(b.getCommandSet() == 'G' && b.getCommand() == 1)
 			{
-				for (Segment s : b)
-				{
-					//If the read in segment is the Z coordinate
-					if (s.getLetter() == 'Z')
-					{
-						//reset newz
-						newz = s.getNumber();
-		//if the difference between the two zs, turn the knob CW in interval of 1
-						if (newz - z > 0)
-						{
-							for (double i = z; i < newz; i ++)
-							{
-								RCW (255,(int) ((newz - z) * 187));
-							}
-						}
-		//else, if the difference is negative, we turn in counter clockwise
-						else if (newz - z < 0)
-						{
-							for (double i = z; i < newz; i ++)
-							{
-								RCCW (255,(int) ((newz - z) * 187));
-							}
-						}
-				//reset the newz
-						z = newz;
-					}
-					//If the read in segment is the x coordinate
-					if (s.getLetter() == 'X')
-					{
-						//reset newx
-						newx = s.getNumber();
-			//if the difference between the two xs, turn the knob CW in interval of 1
-						if (newx - x > 0)
-						{
-							for (double i = x; i < newx; i ++)
-							{
-								RCW ((int)((newx - x) * 187) , 255);
-							}
-						}
-			//else, if the difference is negative, we turn in counter clockwise
-						else if (newx - x < 0)
-						{
-							for (double i = x; i < newx; i ++)
-							{
-								RCCW ((int)((newx - x) * 187) , 255);
-							}
-						}
-			//reset the newx
-						x = newx;
-					}
-				}
+				underwear();
+			}
+			
+			if(b.getCommandSet() == 'G' && (b.getCommand() == 2 || b.getCommand() == 3))
+			{
 
 			}
 		}
 	}
+//underwear is a method that processes little linear segments
+	public void underwear()
+	{
+		double x = 0, z = 0;
+		double newx, newz;
+		for (Block b : program)
+		{
+
+			for (Segment s : b)
+			{
+				//If the read in segment is the Z coordinate
+				if (s.getLetter() == 'Z')
+				{
+					//reset newz
+					newz = s.getNumber();
+					//if the difference between the two zs, turn the knob CW in interval of 1
+					if (newz - z > 0)
+					{
+						for (double i = z; i < newz; i ++)
+						{
+							RCW (255,(int) ((newz - z) * 187));
+						}
+					}
+					//else, if the difference is negative, we turn in counter clockwise
+					else if (newz - z < 0)
+					{
+						for (double i = z; i < newz; i ++)
+						{
+							RCCW (255,(int) ((newz - z) * 187));
+						}
+					}
+					//reset the newz
+					z = newz;
+				}
+				//If the read in segment is the x coordinate
+				if (s.getLetter() == 'X')
+				{
+					//reset newx
+					newx = s.getNumber();
+					//if the difference between the two xs, turn the knob CW in interval of 1
+					if (newx - x > 0)
+					{
+						for (double i = x; i < newx; i ++)
+						{
+							RCW ((int)((newx - x) * 187) , 255);
+						}
+					}
+					//else, if the difference is negative, we turn in counter clockwise
+					else if (newx - x < 0)
+					{
+						for (double i = x; i < newx; i ++)
+						{
+							RCCW ((int)((newx - x) * 187) , 255);
+						}
+					}
+					//reset the newx
+					x = newx;
+				}
+			}
+
+		}
+	}
 }
+
