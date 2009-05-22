bee7c048ca2ea42b004419d66b7fb0bc642369e4
Alexanderngan@stmarksschool.org <Alexanderngan@stmarksschool.org@de70bf9e-f316-11dd-9fdb-3debd5008f28>
2009-05-22 18:26:43 +0000

    this update is once again a step close to gettign the curves working. Now we have the radius!

--- src/gcode/Translator.java
+++ src/gcode/Translator.java
@@ -50,12 +50,65 @@ public class Translator extends Arduino
 		{
 			if(b.getCommandSet() == 'G' && b.getCommand() == 1)
 			{
-				underwear();
+				double x = 0, z = 0;
+				double newx, newz;
+
+				for (Segment s : b)
+				{
+					//If the read in segment is the Z coordinate
+					if (s.getLetter() == 'Z')
+					{
+						//reset newz
+						newz = s.getNumber();
+						//if the difference between the two zs, turn the knob CW in interval of 1
+						if (newz - z > 0)
+						{
+							for (double i = z; i < newz; i ++)
+							{
+								RCW (255,(int) ((newz - z) * 187));
+							}
+						}
+						//else, if the difference is negative, we turn in counter clockwise
+						else if (newz - z < 0)
+						{
+							for (double i = z; i < newz; i ++)
+							{
+								RCCW (255,(int) ((newz - z) * 187));
+							}
+						}
+						//reset the newz
+						z = newz;
+					}
+					//If the read in segment is the x coordinate
+					if (s.getLetter() == 'X')
+					{
+						//reset newx
+						newx = s.getNumber();
+						//if the difference between the two xs, turn the knob CW in interval of 1
+						if (newx - x > 0)
+						{
+							for (double i = x; i < newx; i ++)
+							{
+								RCW ((int)((newx - x) * 187) , 255);
+							}
+						}
+						//else, if the difference is negative, we turn in counter clockwise
+						else if (newx - x < 0)
+						{
+							for (double i = x; i < newx; i ++)
+							{
+								RCCW ((int)((newx - x) * 187) , 255);
+							}
+						}
+						//reset the newx
+						x = newx;
+					}
+				}
 			}
-			
+
 			if(b.getCommandSet() == 'G' && (b.getCommand() == 2 || b.getCommand() == 3))
 			{
-				double arcx, arcy;
+				double arcx = 0, arcy = 0, X = 0, Y = 0, distance, a;
 				for (Segment s : b)
 				{
 					if (s.getLetter() == 'I')
@@ -66,71 +119,24 @@ public class Translator extends Arduino
 					{
 						arcy = s.getNumber();
 					}
+					if (s.getLetter() == 'X')
+					{
+						X = s.getNumber();
+					}
+					if (s.getLetter() == 'Y')
+					{
+						Y = s.getNumber();
+					}
 				}
+				//finding the distance from the center of the circle to the side.
+				a= Math.pow(((arcx -X)), 2) + Math.pow((arcy-Y), 2);
+				distance = Math.sqrt(a);
 			}
 		}
 	}
-//underwear is a method that processes little linear segments
-	public void underwear()
-	{
-		double x = 0, z = 0;
-		double newx, newz;
-		for (Block b : program)
-		{
 
-			for (Segment s : b)
-			{
-				//If the read in segment is the Z coordinate
-				if (s.getLetter() == 'Z')
-				{
-					//reset newz
-					newz = s.getNumber();
-					//if the difference between the two zs, turn the knob CW in interval of 1
-					if (newz - z > 0)
-					{
-						for (double i = z; i < newz; i ++)
-						{
-							RCW (255,(int) ((newz - z) * 187));
-						}
-					}
-					//else, if the difference is negative, we turn in counter clockwise
-					else if (newz - z < 0)
-					{
-						for (double i = z; i < newz; i ++)
-						{
-							RCCW (255,(int) ((newz - z) * 187));
-						}
-					}
-					//reset the newz
-					z = newz;
-				}
-				//If the read in segment is the x coordinate
-				if (s.getLetter() == 'X')
-				{
-					//reset newx
-					newx = s.getNumber();
-					//if the difference between the two xs, turn the knob CW in interval of 1
-					if (newx - x > 0)
-					{
-						for (double i = x; i < newx; i ++)
-						{
-							RCW ((int)((newx - x) * 187) , 255);
-						}
-					}
-					//else, if the difference is negative, we turn in counter clockwise
-					else if (newx - x < 0)
-					{
-						for (double i = x; i < newx; i ++)
-						{
-							RCCW ((int)((newx - x) * 187) , 255);
-						}
-					}
-					//reset the newx
-					x = newx;
-				}
-			}
 
-		}
-	}
+
+
 }
 
